9a9fe09384f96e4624f303c63a500d9b3d949039
Abdallah Masri <abdallahm@sanadcash.com>
2024-08-25 15:42:28 +0300

    25/8

--- src/app/components/task/list.partial.ts
+++ src/app/components/task/list.partial.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
-import   
- { MatCardModule } from '@angular/material/card';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginator, MatPaginatorModule, PageEvent   
- } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import   
- { RouterModule } from '@angular/router';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { RouterModule } from '@angular/router';
 import { ITask } from 'src/app/models/task.model';
 
 @Component({
@@ -20,35 +16,28 @@ import { ITask } from 'src/app/models/task.model';
 export class TaskListPartial {
   @Input() tasks: ITask[];
 
-  pageEvent: PageEvent;
-
   pageIndex = 0;
+  pageSize = 3;
 
-  i = 0;
-
-  handlePageEvent(e: PageEvent){
-    this.pageEvent = e;
+  handlePageEvent(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
   }
 
   get sorted(): ITask[] {
     if (this.tasks) {
       return this.tasks.sort((a, b) => this.compareTasks(a, b)).filter(t => t.status !== "Completed");
     } else {
-      return null;
+      return [];
     }
   }
 
   // Method to compare tasks based on priority and due date
   compareTasks(task1: ITask, task2: ITask): number {
-    const priorityOrder =
-      this.getPriorityOrder(task1.priority) -
-      this.getPriorityOrder(task2.priority);
+    const priorityOrder = this.getPriorityOrder(task1.priority) - this.getPriorityOrder(task2.priority);
     if (priorityOrder !== 0) {
       return priorityOrder;
     }
-    return (
-      new Date(task1.dueDate).getTime() - new Date(task2.dueDate).getTime()
-    );
+    return new Date(task1.dueDate).getTime() - new Date(task2.dueDate).getTime();
   }
 
   // Method to assign a numeric value to each priority for comparison
@@ -63,17 +52,12 @@ export class TaskListPartial {
     }
   }
 
-  goToPreviousPage() {
-    this.pageIndex--;
-  }
-
-  goToNextPage() {
-    this.pageIndex++;
-  }
-
   tasksLength(): number {
-    return this.tasks
-      .filter(task => task.status !== 'Completed')
-      .length;
+    return this.tasks?.filter(task => task.status !== 'Completed').length || 0;
+  }
+
+  get paginatedTasks(): ITask[] {
+    const startIndex = this.pageIndex * this.pageSize;
+    return this.sorted.slice(startIndex, startIndex + this.pageSize);
   }
 }
