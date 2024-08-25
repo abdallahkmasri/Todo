404d0c25871f74da9584ecf8c0c90d4f2ae651aa
Abdallah Masri <abdallahm@sanadcash.com>
2024-08-25 14:48:52 +0300

    25/8

--- src/app/components/task/list.partial.ts
+++ src/app/components/task/list.partial.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { ITask } from 'src/app/models/task.model';
@@ -12,13 +11,10 @@ import { ITask } from 'src/app/models/task.model';
   templateUrl: './list.partial.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, MatCardModule, MatIconModule, RouterModule, MatPaginatorModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, RouterModule],
 })
-export class TaskListPartial implements OnInit{
+export class TaskListPartial{
   @Input() tasks: ITask[];
-
-  dataSource!: MatTableDataSource<ITask>;
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   get sorted(): ITask[] {
     if (this.tasks) {
@@ -41,10 +37,6 @@ export class TaskListPartial implements OnInit{
     );
   }
 
-  ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.sorted);
-  }
-
   // Method to assign a numeric value to each priority for comparison
   getPriorityOrder(priority: string): number {
     switch (priority) {
@@ -55,9 +47,5 @@ export class TaskListPartial implements OnInit{
       default:
         return 3;
     }
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator
   }
 }
